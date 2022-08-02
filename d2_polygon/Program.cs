43ea689e92669b4e3d700e9f2dddde54a608ab8c
Hikanyan <hikanyan0131@gmail.com>
2022-08-02 20:37:52 +0900

    プロジェクト ファイルを追加します。

---
+++ d2_polygon/Program.cs
@@ -0,0 +1,14 @@
+﻿//https://paiza.jp/works/mondai/drankfast/d2_polygon
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        var line = Console.ReadLine();
+        Console.WriteLine("XXXXXX");
+    }
+}
